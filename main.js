a97f0725a42a6d63d4900fae7cd8d02fb797f187
trinhuyenlht7 <trinhuyenlht7@gmail.com>
2024-03-28 11:15:37 +0700

    first push

---
+++ main.js
@@ -0,0 +1,16 @@
+// function firstFunction(uyen) {
+//     console.log('uyen đẹp trai')
+// }
+
+// firstFunction()
+
+// function showDialog(){
+//     alert ('Hi xinh chào các bạn !')
+// }
+
+// showDialog()
+// function writeLog(){
+     
+// }
+
+
