ba957599ad9043a2c6e33fc070ea426f81e57b2c
trinhuyenlht7 <trinhuyenlht7@gmail.com>
2024-04-04 02:32:57 +0700

    thu vy la con cho

--- float.js
+++ float.js
@@ -1,1 +1,11 @@
-alert('Thu Vy Khùm có đúng không')
+alert('bạn chọn 1 hoặc 2 nha')
+alert('chọn 1 là đồng ý Thu Vy là chó')
+alert('chọn số khác bất kì là không đồng ý Thu Vy là chó')
+
+do{
+    var a = prompt('mời bạn nhập lựa chọn ạ')
+    if(a==1){
+        alert('Bạn đã đúng, Thu Vy là chó')
+    }else 
+        alert('Bạn đã chọn sai, mời bạn chọn lại ạ')
+}while(a!=1)
